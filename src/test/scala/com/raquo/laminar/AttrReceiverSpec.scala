4f9f83e9d17d29040c11e49e25ae87f9cf84dc09
Nikita Gazarov <nikita@raquo.com>
2017-03-23 20:51:05 -0700

    Test: add more tests for AttrReceiver

--- src/test/scala/com/raquo/laminar/AttrReceiverSpec.scala
+++ src/test/scala/com/raquo/laminar/AttrReceiverSpec.scala
@@ -17,7 +17,6 @@ class AttrReceiverSpec extends UnitSpec {
     val title1 = randomString("title1_")
     val title2 = randomString("title2_")
     val title3 = randomString("title3_")
-    val title4 = randomString("title4_")
     val $title = XStream.create[String]()
     val $writeableTitle = new ShamefulStream($title)
 
@@ -35,18 +34,18 @@ class AttrReceiverSpec extends UnitSpec {
 
     $writeableTitle.shamefullySendNext(title3)
     expectElement(div like (cls is "unrelated"))
-
-    $writeableTitle.shamefullySendNext(title4)
-    expectElement(div like (cls is "unrelated"))
   }
+
+//  it("updates attr with multiple streams on same attr") {
+//    // @TODO This isn't really a published feature, should we really test for it?
+//  }
 
   it("updates attr with memory stream") {
     val title1 = randomString("title1_")
     val title2 = randomString("title2_")
     val title3 = randomString("title3_")
     val title4 = randomString("title4_")
-    val title5 = randomString("title5_")
-    val $title: MemoryStream[String, Nothing] = XStream.create[String]().startWith(title1)
+    val $title = XStream.create[String]().startWith(title1)
     val $writeableTitle = new ShamefulStream($title)
 
     mount(div(title <-- $title, "Hello"))
@@ -63,13 +62,116 @@ class AttrReceiverSpec extends UnitSpec {
 
     $writeableTitle.shamefullySendNext(title4)
     expectElement(div like (cls is "unrelated"))
+  }
+
+  it("supports multiple attr receivers on same node") {
+    val title1 = randomString("title1_")
+    val title2 = randomString("title2_")
+    val title3 = randomString("title3_")
+    val title4 = randomString("title4_")
+    val title5 = randomString("title5_")
+    val title6 = randomString("title6_")
+    val $title = XStream.create[String]()
+    val $writeableTitle = new ShamefulStream($title)
+
+    val rel1 = randomString("rel1_")
+    val rel2 = randomString("rel2_")
+    val rel3 = randomString("rel3_")
+    val rel4 = randomString("rel4_")
+    val rel5 = randomString("rel5_")
+    val $rel = XStream.create[String]()
+    val $writeableRel = new ShamefulStream($rel)
+
+    mount(div(title <-- $title, rel <-- $rel, "Hello"))
+    expectElement(div like "Hello")
+
+    $writeableTitle.shamefullySendNext(title1)
+    expectElement(div like (title is title1, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel1)
+    expectElement(div like (title is title1, rel is rel1, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title2)
+    expectElement(div like (title is title2, rel is rel1, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title3)
+    expectElement(div like (title is title3, rel is rel1, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel2)
+    expectElement(div like (title is title3, rel is rel2, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title4)
+    expectElement(div like (title is title4, rel is rel2, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel3)
+    expectElement(div like (title is title4, rel is rel3, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel4)
+    expectElement(div like (title is title4, rel is rel4, "Hello"))
 
     $writeableTitle.shamefullySendNext(title5)
+    expectElement(div like (title is title5, rel is rel4, "Hello"))
+
+    patchMounted(div(cls := "unrelated"))
+    expectElement(div like (cls is "unrelated"))
+
+    $writeableTitle.shamefullySendNext(title6)
+    expectElement(div like (cls is "unrelated"))
+
+    $writeableRel.shamefullySendNext(rel5)
     expectElement(div like (cls is "unrelated"))
   }
 
-  it("works with another attr receiver on same node") {
+  it("supports multiple memory streams") {
+    val title1 = randomString("title1_")
+    val title2 = randomString("title2_")
+    val title3 = randomString("title3_")
+    val title4 = randomString("title4_")
+    val title5 = randomString("title5_")
+    val title6 = randomString("title6_")
+    val $title = XStream.create[String]().startWith(title1)
+    val $writeableTitle = new ShamefulStream($title)
 
+    val rel1 = randomString("rel1_")
+    val rel2 = randomString("rel2_")
+    val rel3 = randomString("rel3_")
+    val rel4 = randomString("rel4_")
+    val rel5 = randomString("rel5_")
+    val $rel = XStream.create[String]().startWith(rel1)
+    val $writeableRel = new ShamefulStream($rel)
+
+    mount(div(title <-- $title, rel <-- $rel, "Hello"))
+    expectElement(div like (title is title1, rel is rel1, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title2)
+    expectElement(div like (title is title2, rel is rel1, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title3)
+    expectElement(div like (title is title3, rel is rel1, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel2)
+    expectElement(div like (title is title3, rel is rel2, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title4)
+    expectElement(div like (title is title4, rel is rel2, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel3)
+    expectElement(div like (title is title4, rel is rel3, "Hello"))
+
+    $writeableRel.shamefullySendNext(rel4)
+    expectElement(div like (title is title4, rel is rel4, "Hello"))
+
+    $writeableTitle.shamefullySendNext(title5)
+    expectElement(div like (title is title5, rel is rel4, "Hello"))
+
+    patchMounted(div(cls := "unrelated"))
+    expectElement(div like (cls is "unrelated"))
+
+    $writeableTitle.shamefullySendNext(title6)
+    expectElement(div like (cls is "unrelated"))
+
+    $writeableRel.shamefullySendNext(rel5)
+    expectElement(div like (cls is "unrelated"))
   }
 
   it("works with child receiver on same node") {
