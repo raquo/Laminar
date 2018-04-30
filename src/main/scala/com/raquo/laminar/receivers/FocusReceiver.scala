db97778d5dd9d8993d50b52136f490abe9b2bbfd
Nikita Gazarov <nikita@raquo.com>
2018-04-29 21:49:56 -0700

    Api: FocusReceiver now accepts streams only
    It does not make sense for it to accept other reactive vars since it can't maintain that state

--- src/main/scala/com/raquo/laminar/receivers/FocusReceiver.scala
+++ src/main/scala/com/raquo/laminar/receivers/FocusReceiver.scala
@@ -1,20 +1,20 @@
 package com.raquo.laminar.receivers
 
-import com.raquo.airstream.core.Observable
+import com.raquo.airstream.eventstream.EventStream
 import com.raquo.laminar.nodes.ReactiveHtmlElement
 import com.raquo.laminar.setters.FocusSetter
 import org.scalajs.dom
 
 class FocusReceiver(val element: ReactiveHtmlElement[dom.html.Element]) extends AnyVal {
 
-  @inline def <--($isFocused: Observable[Boolean]): Unit = {
+  @inline def <--($isFocused: EventStream[Boolean]): Unit = {
     (FocusReceiver <-- $isFocused)(element)
   }
 }
 
 object FocusReceiver {
 
-  @inline def <--($isFocused: Observable[Boolean]): FocusSetter = {
+  @inline def <--($isFocused: EventStream[Boolean]): FocusSetter = {
     new FocusSetter($isFocused)
   }
 }
