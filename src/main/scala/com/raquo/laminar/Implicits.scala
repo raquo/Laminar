7bb49930912eaf4d5c3dc3031e18c45c0991dfc8
Nikita Gazarov <nikita@raquo.com>
2018-06-15 00:26:36 -0700

    API: Implicitly convert Option[Modifier[A]] to Modifier[A]

--- src/main/scala/com/raquo/laminar/Implicits.scala
+++ src/main/scala/com/raquo/laminar/Implicits.scala
@@ -26,11 +26,22 @@ trait Implicits extends DomApi {
     new ReactiveText(text)
   }
 
+  /** Create a modifier that applies each of the modifiers in a seq */
   implicit def seqToModifier[A, El <: ReactiveElement[dom.Element]](seq: Seq[A])(implicit evidence: A => Modifier[El]): Modifier[El] = {
     // @TODO[Performance] See if we might want a separate implicit conversion for cases when we don't need `evidence`
     new Modifier[El] {
       override def apply(el: El): Unit = {
         seq.foreach(evidence(_).apply(el))
+      }
+    }
+  }
+
+  /** Create a modifier that applies the modifier in an option, if it's defined, or does nothing otherwise */
+  implicit def optionToModifier[A, El <: ReactiveElement[dom.Element]](option: Option[A])(implicit evidence: A => Modifier[El]): Modifier[El] = {
+    // @TODO[Performance] See if we might want a separate implicit conversion for cases when we don't need `evidence`
+    new Modifier[El] {
+      override def apply(el: El): Unit = {
+        option.foreach(evidence(_).apply(el))
       }
     }
   }
