fa27b01c57133f7f51f56cb903402858e79947fe
Nikita Gazarov <nikita@raquo.com>
2020-02-07 17:47:38 -0800

    API: Replace auxiliary subscription syntax with `amend` method

--- src/main/scala/com/raquo/laminar/nodes/ReactiveHtmlElement.scala
+++ src/main/scala/com/raquo/laminar/nodes/ReactiveHtmlElement.scala
@@ -2,22 +2,12 @@ package com.raquo.laminar.nodes
 
 import com.raquo.laminar.DomApi
 import com.raquo.laminar.builders.HtmlTag
-import com.raquo.laminar.keys.{ReactiveHtmlAttr, ReactiveProp, ReactiveStyle}
-import com.raquo.laminar.receivers.{AttrReceiver, FocusReceiver, PropReceiver, StyleReceiver}
 import org.scalajs.dom
 
 class ReactiveHtmlElement[+Ref <: dom.html.Element](val tag: HtmlTag[Ref])
   extends ReactiveElement[Ref] {
 
   final override val ref: Ref = DomApi.createHtmlElement(this)
-
-  final def <--[V](style: ReactiveStyle[V]): StyleReceiver[V] = new StyleReceiver(style, this)
-
-  final def <--[V](attr: ReactiveHtmlAttr[V]): AttrReceiver[V] = new AttrReceiver(attr, this)
-
-  final def <--[V, DomV](prop: ReactiveProp[V, DomV]): PropReceiver[V, DomV] = new PropReceiver(prop, this)
-
-  final def <--[V](focus: FocusReceiver.type): FocusReceiver = new FocusReceiver(this)
 
   override def toString: String = {
     // `ref` is not available inside ReactiveElement's constructor due to initialization order, so fall back to `tag`.
--- src/main/scala/com/raquo/laminar/nodes/ReactiveSvgElement.scala
+++ src/main/scala/com/raquo/laminar/nodes/ReactiveSvgElement.scala
@@ -2,16 +2,12 @@ package com.raquo.laminar.nodes
 
 import com.raquo.laminar.DomApi
 import com.raquo.laminar.builders.SvgTag
-import com.raquo.laminar.keys.ReactiveSvgAttr
-import com.raquo.laminar.receivers.SvgAttrReceiver
 import org.scalajs.dom
 
 class ReactiveSvgElement[+Ref <: dom.svg.Element](val tag: SvgTag[Ref])
   extends ReactiveElement[Ref] {
 
   final override val ref: Ref = DomApi.createSvgElement(this)
-
-  final def <--[V](attr: ReactiveSvgAttr[V]): SvgAttrReceiver[V] = new SvgAttrReceiver(attr, this)
 
   override def toString: String = {
     // `ref` is not available inside ReactiveElement's constructor due to initialization order, so fall back to `tag`.
