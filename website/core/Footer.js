f1a3f1ddb7125c5e85c0034547df3df6832761ca
Nikita Gazarov <nikita@raquo.com>
2020-10-11 16:49:22 -0700

    Docs: Add basic analytics to website

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -65,6 +65,17 @@ class Footer extends React.Component {
           </div>
         </section>
         <section className="copyright">{this.props.config.copyright}</section>
+        <script dangerouslySetInnerHTML={{__html: `
+          <!-- Basic Woopra analytics -->
+          (function(){
+            var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+          })("woopra");
+          woopra.config({
+            domain: 'laminar.dev'
+          });
+          woopra.track();
+          
+        `}} />
       </footer>
     );
   }
