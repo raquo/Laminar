e2c15b8d78c22f45d401aae9c4981bcace8c5a49
Nikita Gazarov <nikita@raquo.com>
2021-01-31 02:30:51 -0800

    Build: Update versions and release flow

---
+++ project/Versions.scala
@@ -0,0 +1,22 @@
+object Versions {
+
+  val Scala_2_13 = "2.13.4"
+
+  val Scala_2_12 = "2.12.12"
+
+  val Scala_3_RC1 = "3.0.0-RC1-bin-20210113-8345078-NIGHTLY"
+
+  val ScalaJsDom = "1.1.0"
+
+  val ScalaDomTypes = "0.12.0"
+
+  val Airstream = "0.12.0-M1"
+
+  // Testing
+
+  val ScalaTest = "3.2.0"
+
+  val JsDom = "16.4.0"
+
+  val ScalaDomTestUtils = "0.14.1"
+}
