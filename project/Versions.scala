ad798f58d22cc07e6e05d734f5f275f040c479fa
Nikita Gazarov <nikita@raquo.com>
2021-02-21 02:02:53 -0800

    Build: Use published versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,13 +4,13 @@ object Versions {
 
   val Scala_2_12 = "2.12.12"
 
-  val Scala_3_RC1 = "3.0.0-RC1-bin-20210113-8345078-NIGHTLY"
+  val Scala_3_RC1 = "3.0.0-RC1"
 
   val ScalaJsDom = "1.1.0"
 
   val ScalaDomTypes = "0.13.0"
 
-  val Airstream = "0.12.0-SNAPSHOT"
+  val Airstream = "0.12.0-RC1"
 
   // Testing
 
@@ -18,5 +18,5 @@ object Versions {
 
   val JsDom = "16.4.0"
 
-  val ScalaDomTestUtils = "0.14.3-SNAPSHOT"
+  val ScalaDomTestUtils = "0.14.3"
 }
