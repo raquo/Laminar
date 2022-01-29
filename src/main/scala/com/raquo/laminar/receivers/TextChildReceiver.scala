12f6233627397e32861927bd8df8c18affd9b930
Nikita Gazarov <nikita@raquo.com>
2022-01-28 19:18:53 -0800

    New: Special TextInserter implementation
    More efficient, and with correct auto-distinction

--- src/main/scala/com/raquo/laminar/receivers/TextChildReceiver.scala
+++ src/main/scala/com/raquo/laminar/receivers/TextChildReceiver.scala
@@ -1,17 +1,14 @@
 package com.raquo.laminar.receivers
 
 import com.raquo.airstream.core.Source
-import com.raquo.laminar.modifiers.{ChildInserter, Inserter}
+import com.raquo.laminar.modifiers.{Inserter, TextInserter}
 import com.raquo.laminar.nodes.{ReactiveElement, TextNode}
 
 object TextChildReceiver {
 
-  // @TODO[Performance] We create new text nodes. Should we update the node's text instead? Also check how we treat comments.
+  // @TODO[Performance] We create new text nodes for every event. Should we update the node's text instead?
 
-  def <--[T]($node: Source[T])(implicit ev: T => TextNode): Inserter[ReactiveElement.Base] = {
-    ChildInserter[ReactiveElement.Base](
-      _ => $node.toObservable.map(ev(_)),
-      initialInsertContext = None
-    )
+  def <--[T]($node: Source[T])(implicit render: T => TextNode): Inserter[ReactiveElement.Base] = {
+    TextInserter[T](_ => $node.toObservable, render)
   }
 }
