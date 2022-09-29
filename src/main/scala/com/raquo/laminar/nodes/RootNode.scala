raquo/Laminar

commit 66f559cc14bcb4f077715822ee2d80d8d3063aaf

Misc: Remove duplicate mount check in RootNode

src/main/scala/com/raquo/laminar/nodes/RootNode.scala
@@ -28,6 +28,8 @@ class RootNode(
     throw new Exception("Unable to mount Laminar RootNode into an unmounted container.")
   }
 
+  mount()
+
   /** When we create a Root, we don't want to create a new HTML Element, we want to
     * use a reference to an existing element, the container.
     */
@@ -45,7 +47,4 @@ class RootNode(
     ParentNode.removeChild(parent = this, child = child)
   }
 
-  if (ChildNode.isNodeMounted(container)) {
-    mount()
-  }
 }

commit 143d9abafb3ef5e486935ed63f23a7c669fcac50

Fix: mount() must come AFTER val ref in RootNode
The error was introduced in 66f559cc14bcb4f077715822ee2d80d8d3063aaf

src/main/scala/com/raquo/laminar/nodes/RootNode.scala
@@ -28,12 +28,12 @@ class RootNode(
     throw new Exception("Unable to mount Laminar RootNode into an unmounted container.")
   }
 
-  mount()
-
   /** When we create a Root, we don't want to create a new HTML Element, we want to
     * use a reference to an existing element, the container.
     */
   final override val ref: dom.Element = container
+
+  mount()
 
   /** @return Whether child was successfully mounted */
   def mount(): Boolean = {