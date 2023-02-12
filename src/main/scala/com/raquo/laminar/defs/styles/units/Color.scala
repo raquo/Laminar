b44103a9737eca71c12606d2d0a75890e7cab3ca
Nikita Gazarov <nikita@raquo.com>
2023-02-11 21:08:55 -0700

    API: Use better representations of rgb and hsl CSS functions
    Apparently the rgba / hsla methods are legacy, and rgb / hsl support alpha parameter. Having overloaded methods instead of 1.0 default for alpha makes eta expansion behave nicer

--- src/main/scala/com/raquo/laminar/defs/styles/units/Color.scala
+++ src/main/scala/com/raquo/laminar/defs/styles/units/Color.scala
@@ -6,48 +6,70 @@ import com.raquo.laminar.keys.DerivedStyleBuilder
 
 trait Color[SS, DSP[_]] { this: DerivedStyleBuilder[SS, DSP] =>
 
-  /**
-   * @param red   0..255
-   * @param green 0..255
-   * @param blue  0..255
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/rgb
-   */
+  /** @param red   0..255
+    * @param green 0..255
+    * @param blue  0..255
+    *
+    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/rgb
+    */
   def rgb(red: Int, green: Int, blue: Int): SS =
     styleSetter(s"rgb($red, $green, $blue)")
 
 
-  /**
-   * @param red   0..255
-   * @param green 0..255
-   * @param blue  0..255
-   * @param alpha 0..1, indicating opacity (0 means transparent)
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/rgba
-   */
+  /** @param red   0..255
+    * @param green 0..255
+    * @param blue  0..255
+    * @param alpha 0..1, indicating opacity (0 means transparent)
+    *
+    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/rgb
+    */
+  def rgb(red: Int, green: Int, blue: Int, alpha: Double): SS = {
+    val alphaStr = if (alpha < 0.999999999) s" / $alpha" else ""
+    styleSetter(s"rgb($red $green $blue$alphaStr)")
+  }
+
+
+  /** @param red   0..255
+    * @param green 0..255
+    * @param blue  0..255
+    * @param alpha 0..1, indicating opacity (0 means transparent)
+    *
+    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/rgba
+    */
   def rgba(red: Int, green: Int, blue: Int, alpha: Double): SS =
     styleSetter(s"rgba($red, $green, $blue, $alpha)")
 
 
-  /**
-   * @param hue        0..360
-   * @param saturation 0..100
-   * @param lightness  0..100
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsl
-   */
+  /** @param hue        0..360
+    * @param saturation 0..100
+    * @param lightness  0..100
+    *
+    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsl
+    */
   def hsl(hue: Double, saturation: Double, lightness: Double): SS =
     styleSetter(s"hsl($hue, $saturation%, $lightness%)")
 
 
-  /**
-   * @param hue        0..360
-   * @param saturation 0..100
-   * @param lightness  0..100
-   * @param alpha      0..1, indicating opacity (0 means transparent)
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsla
-   */
+  /** @param hue        0..360
+    * @param saturation 0..100
+    * @param lightness  0..100
+    * @param alpha      0..1, indicating opacity (0 means transparent)
+    *
+    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsl
+    */
+  def hsl(hue: Double, saturation: Double, lightness: Double, alpha: Double): SS = {
+    val alphaStr = if (alpha < 0.999999999) s" / $alpha" else ""
+    styleSetter(s"hsl($hue $saturation% $lightness%$alphaStr)")
+  }
+
+
+  /** @param hue        0..360
+    * @param saturation 0..100
+    * @param lightness  0..100
+    * @param alpha      0..1, indicating opacity (0 means transparent)
+    *
+    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsla
+    */
   def hsla(hue: Double, saturation: Double, lightness: Double, alpha: Double): SS =
     styleSetter(s"hsla($hue, $saturation%, $lightness%, $alpha)")
 
