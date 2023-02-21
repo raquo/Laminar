14960c9e18265728c6d773d039652f86ec1801b4
Nikita Gazarov <nikita@raquo.com>
2023-02-21 14:06:12 -0700

    API: More efficient representation for Codecs

--- src/main/scala/com/raquo/laminar/codecs/package.scala
+++ src/main/scala/com/raquo/laminar/codecs/package.scala
@@ -4,13 +4,14 @@ package object codecs {
 
   // String Codecs
 
-  object StringAsIsCodec extends AsIsCodec[String]
+  val StringAsIsCodec: AsIsCodec[String] = AsIsCodec()
 
   // Int Codecs
 
-  object IntAsIsCodec extends AsIsCodec[Int]
+  val IntAsIsCodec: AsIsCodec[Int] = AsIsCodec()
 
-  object IntAsStringCodec extends Codec[Int, String] {
+  lazy val IntAsStringCodec: Codec[Int, String] = new Codec[Int, String] {
+
     override def decode(domValue: String): Int = domValue.toInt // @TODO this can throw exception. How do we handle this?
 
     override def encode(scalaValue: Int): String = scalaValue.toString
@@ -18,9 +19,10 @@ package object codecs {
 
   // Double Codecs
 
-  object DoubleAsIsCodec extends AsIsCodec[Double]
+  lazy val DoubleAsIsCodec: AsIsCodec[Double] = AsIsCodec()
 
-  object DoubleAsStringCodec extends Codec[Double, String] {
+  lazy val DoubleAsStringCodec: Codec[Double, String] = new Codec[Double, String] {
+
     override def decode(domValue: String): Double = domValue.toDouble // @TODO this can throw exception. How do we handle this?
 
     override def encode(scalaValue: Double): String = scalaValue.toString
@@ -28,27 +30,31 @@ package object codecs {
 
   // Boolean Codecs
 
-  object BooleanAsIsCodec extends AsIsCodec[Boolean]
+  val BooleanAsIsCodec: AsIsCodec[Boolean] = AsIsCodec[Boolean]()
 
-  object BooleanAsAttrPresenceCodec extends Codec[Boolean, String] {
+  val BooleanAsAttrPresenceCodec: Codec[Boolean, String] = new Codec[Boolean, String] {
+
     override def decode(domValue: String): Boolean = domValue != null
 
     override def encode(scalaValue: Boolean): String = if (scalaValue) "" else null
   }
 
-  object BooleanAsTrueFalseStringCodec extends Codec[Boolean, String] {
+  lazy val BooleanAsTrueFalseStringCodec: Codec[Boolean, String] = new Codec[Boolean, String] {
+
     override def decode(domValue: String): Boolean = domValue == "true"
 
     override def encode(scalaValue: Boolean): String = if (scalaValue) "true" else "false"
   }
 
-  object BooleanAsYesNoStringCodec extends Codec[Boolean, String] {
+  lazy val BooleanAsYesNoStringCodec: Codec[Boolean, String] = new Codec[Boolean, String] {
+
     override def decode(domValue: String): Boolean = domValue == "yes"
 
     override def encode(scalaValue: Boolean): String = if (scalaValue) "yes" else "no"
   }
 
-  object BooleanAsOnOffStringCodec extends Codec[Boolean, String] {
+  lazy val BooleanAsOnOffStringCodec: Codec[Boolean, String] = new Codec[Boolean, String] {
+
     override def decode(domValue: String): Boolean = domValue == "on"
 
     override def encode(scalaValue: Boolean): String = if (scalaValue) "on" else "off"
@@ -56,13 +62,17 @@ package object codecs {
 
   // Iterable Codecs
 
-  object IterableAsSpaceSeparatedStringCodec extends Codec[Iterable[String], String] { // use for e.g. className
+  @deprecated("Laminar no longer uses IterableAsSpaceSeparatedStringCodec so I plan to remove it; if you need it, please let me know", "15.0.0-M7")
+  lazy val IterableAsSpaceSeparatedStringCodec: Codec[Iterable[String], String] = new Codec[Iterable[String], String] { // could use for e.g. className
+
     override def decode(domValue: String): Iterable[String] = if (domValue == "") Nil else domValue.split(' ')
 
     override def encode(scalaValue: Iterable[String]): String = scalaValue.mkString(" ")
   }
 
-  object IterableAsCommaSeparatedStringCodec extends Codec[Iterable[String], String] { // use for lists of IDs
+  @deprecated("Laminar no longer uses IterableAsCommaSeparatedStringCodec so I plan to remove it; if you need it, please let me know", "15.0.0-M7")
+  lazy val IterableAsCommaSeparatedStringCodec: Codec[Iterable[String], String] = new Codec[Iterable[String], String] { // could use for lists of IDs
+
     override def decode(domValue: String): Iterable[String] = if (domValue == "") Nil else domValue.split(',')
 
     override def encode(scalaValue: Iterable[String]): String = scalaValue.mkString(",")
