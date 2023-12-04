1254a192d131f6ffa41ae650e5b3958e477275b7
Nikita Gazarov <nikita@raquo.com>
2023-12-04 15:29:59 -0800

    Build: Use released versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,24 +2,24 @@ object Versions {
 
   val Scala_2_12 = "2.12.17"
 
-  val Scala_2_13 = "2.13.10"
+  val Scala_2_13 = "2.13.12"
 
   val Scala_3 = "3.3.1"
 
   // -- Dependencies --
 
-  val Airstream = "17.0.0-M2-SNAPSHOT"
+  val Airstream = "17.0.0-M2"
 
-  val Ew = "0.2.0-SNAPSHOT"
+  val Ew = "0.2.0"
 
-  val ScalaJsDom = "2.6.0"
+  val ScalaJsDom = "2.8.0"
 
   // Note: Scala DOM Types is a compile-time dependency now,
   // see its version in project/project/ProjectVersions.scala
 
   // -- Test --
 
-  val ScalaDomTestUtils = "17.1.0-SNAPSHOT"
+  val ScalaDomTestUtils = "18.0.0"
 
   val ScalaTest = "3.2.14"
 
