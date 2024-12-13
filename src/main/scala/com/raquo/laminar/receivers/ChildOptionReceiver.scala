ee7df9b43ed881a0bc86936cf4efd3a73cb8d0c6
Nikita Gazarov <nikita@raquo.com>
2024-12-12 21:11:41 -0800

    Fix: formatting

--- src/main/scala/com/raquo/laminar/receivers/ChildOptionReceiver.scala
+++ src/main/scala/com/raquo/laminar/receivers/ChildOptionReceiver.scala
@@ -9,7 +9,7 @@ import com.raquo.laminar.nodes.{ChildNode, CommentNode}
 object ChildOptionReceiver {
 
   def <--(maybeChildSource: Source[Option[ChildNode.Base]]): DynamicInserter = {
-   lazy val emptyNode = new CommentNode("")
+    lazy val emptyNode = new CommentNode("")
     child <-- maybeChildSource.toObservable.map(_.getOrElse(emptyNode))
   }
 
